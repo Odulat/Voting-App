db6b207cbfc192b1ecd65115230a377c296cc18f
Dulat Orazymbetov <odulati@gmail.com>
2022-03-11 10:59:20 +0600

    Some fixes

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" href="https://img.icons8.com/doodle/96/000000/quizlet.png"/>
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
